a2d58ff447d4734197b103b054bde72a15b43c3a
Andrea Bertagnolli <andrea.bertagnolli@gmail.com>
2018-06-05 13:42:40 +0200

    Add StartFetchCommandHandler to Commands

--- src/main/java/ndr/brt/tradegs/Commands.java
+++ src/main/java/ndr/brt/tradegs/Commands.java
@@ -1,6 +1,12 @@
 package ndr.brt.tradegs;
 
+import ndr.brt.tradegs.user.CreateUser;
 import ndr.brt.tradegs.user.CreateUserHandler;
+import ndr.brt.tradegs.user.StartFetch;
+import ndr.brt.tradegs.user.StartFetchHandler;
+
+import java.util.Map;
+import java.util.Optional;
 
 public interface Commands {
 
@@ -8,20 +14,26 @@ public interface Commands {
         return new Instance();
     }
 
-    <T extends Command> void post(Command command);
+    <T extends Command> void post(T command);
 
     class Instance implements Commands {
 
-        private Handler handler;
+        private final Map<Class, Handler> handlers;
 
-        public Instance() {
-            handler = new CreateUserHandler();
+        private Instance() {
+            handlers = Map.of(
+                    CreateUser.class, new CreateUserHandler(),
+                    StartFetch.class, new StartFetchHandler()
+            );
         }
 
         @SuppressWarnings("unchecked")
         @Override
-        public <T extends Command> void post(Command command) {
-            handler.handle(command);
+        public <T extends Command> void post(T command) {
+            Optional.of(command)
+                    .map(Object::getClass)
+                    .map(handlers::get)
+                    .ifPresent(it -> it.handle(command));
         }
     }
 }
---
+++ src/main/java/ndr/brt/tradegs/user/StartFetchHandler.java
@@ -0,0 +1,10 @@
+package ndr.brt.tradegs.user;
+
+import ndr.brt.tradegs.Handler;
+
+public class StartFetchHandler implements Handler<StartFetch> {
+    @Override
+    public void handle(StartFetch command) {
+
+    }
+}
