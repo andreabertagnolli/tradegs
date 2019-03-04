5647151b508b524396fefafd0460a4b23ff11df7
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2019-03-04 09:07:43 +0100

    Introduce reflections to build event classes map

--- src/main/java/ndr/brt/tradegs/Event.java
+++ src/main/java/ndr/brt/tradegs/Event.java
@@ -4,10 +4,6 @@ import java.time.LocalDateTime;
 
 public class Event {
 
-    public Event() {
-        EventClasses.put(getClass().getSimpleName(), getClass());
-    }
-
     private final LocalDateTime timestamp = LocalDateTime.now();
     private final String type = getClass().getSimpleName();
 
--- src/main/java/ndr/brt/tradegs/EventClasses.java
+++ src/main/java/ndr/brt/tradegs/EventClasses.java
@@ -1,16 +1,26 @@
 package ndr.brt.tradegs;
 
+import org.reflections.ReflectionUtils;
+import org.reflections.Reflections;
+
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.function.Function;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
 
 public enum EventClasses {
     INSTANCE;
 
-    public static void put(String className, Class<? extends Event> clazz) {
-        INSTANCE.map.put(className, clazz);
-    }
-
     private ConcurrentMap<String, Class<? extends Event>> map = new ConcurrentHashMap<>();
+
+    EventClasses() {
+        Reflections reflections = new Reflections("ndr.brt");
+        reflections.getSubTypesOf(Event.class).forEach(it -> map.put(it.getSimpleName(), it));
+    }
 
     public static Class<? extends Event> get(String className) {
         return INSTANCE.map.get(className);
